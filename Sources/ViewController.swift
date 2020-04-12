837cb166ed62ffe7ca6537d606bb1cd97a30f5ce
Prickett <jpricke1@ford.com>
2020-04-12 15:01:46 -0400

    Initial Commit

---
+++ Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechToText-gRPC-iOS
+//
+//  Created by Prickett, Jacob (J.A.) on 4/12/20.
+//  Copyright © 2020 Prickett, Jacob (J.A.). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
