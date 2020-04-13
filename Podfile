348f03f818cbfb2b44c194cd18f73bd50c0a504e
Prickett <jpricke1@ford.com>
2020-04-13 07:48:49 -0400

    Remove Storyboard, Add Generated gRPC Client and Objects

--- Podfile
+++ Podfile
@@ -1,5 +1,7 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '13.0'
+
+inhibit_all_warnings!
 
 target 'SpeechToText-gRPC-iOS' do
   # Comment the next line if you don't want to use dynamic frameworks
--- Sources/SceneDelegate.swift
+++ Sources/SceneDelegate.swift
@@ -11,13 +11,25 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var navController: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        navController = UINavigationController()
+        navController?.navigationBar.backgroundColor = .blue
+        navController?.navigationBar.prefersLargeTitles = false
+
+        let viewController: UIViewController = ViewController()
+        navController?.pushViewController(viewController, animated: false)
+
+        window = UIWindow(windowScene: windowScene)
+        self.window!.rootViewController = navController
+        self.window!.backgroundColor = .gray
+        self.window!.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
